5ba1ad3c07571a26a1f2fbf53884c0f89c3b43e9
girlcalledblu <45247880+girlcalledblu@users.noreply.github.com>
2025-09-23 18:57:55 -0700

    Update custom_char_card.rb

--- plugins/scenes/custom_char_card.rb
+++ plugins/scenes/custom_char_card.rb
@@ -8,11 +8,6 @@ module AresMUSH
           items: Simpleinventory.get_items(char)
       }
     end
-      
-      # Otherwise return a hash of data.  For example, if you want to show traits you could do:
-      # {
-      #   traits: char.traits.map { |k, v| { name: k, description: v } }
-      # }
     end
   end
 end
