62b93b3526cc5960b6df90dffa512c618deb1a00
mscottkey <mscottkey@gmail.com>
2025-10-29 19:29:12 -0700

    Refactor parse_args to handle admin and player cases

--- plugins/pools/commands/pool_reset_cmd.rb
+++ plugins/pools/commands/pool_reset_cmd.rb
@@ -5,10 +5,26 @@ module AresMUSH
       
       attr_accessor :name
 
-      def parse_args
-          self.name = !cmd.args ? enactor_name : titlecase_arg(cmd.args)          
+     def parse_args
+        if Pools.can_manage_pools?(enactor) && (cmd.args =~ /\//)
+          # Admin: <name>=<amount>/<reason>
+          args = cmd.parse_args(ArgParser.arg1_equals_arg2_slash_arg3)
+          self.name   = titlecase_arg(args.arg1)
+          self.amount = integer_arg(args.arg2)
+          self.reason = titlecase_arg(args.arg3)
+        else
+          # Player: <amount>=<reason> (name defaults to self)
+          args = cmd.parse_args(ArgParser.arg1_equals_arg2)
+          self.name   = enactor_name
+          self.amount = integer_arg(args.arg1)
+          self.reason = titlecase_arg(args.arg2)
+        end
       end
-        
+
+      def required_args
+        [ self.name, self.amount, self.reason ]
+      end
+      
       def check_can_reset
         return nil if enactor_name == self.name
         return nil if Pools.can_manage_pools?(enactor)
